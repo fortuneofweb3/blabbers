30d5e1ea53153837b1f60f4666d79a07e4effa20
fortuneofweb3 <138443589+fortuneofweb3@users.noreply.github.com>
2025-06-27 19:34:37 +0100

    Update project.js

--- models/project.js
+++ models/project.js
@@ -1,7 +1,11 @@
 const mongoose = require('mongoose');
-const ProjectSchema = new mongoose.Schema({
+const Schema = mongoose.Schema;
+
+const projectSchema = new Schema({
   name: { type: String, required: true, unique: true },
-  keywords: [String],
-  description: String
+  keywords: { type: [String], default: [] },
+  description: { type: String, default: '' },
+  updatedAt: { type: Date, default: Date.now }
 });
-module.exports = mongoose.model('Project', ProjectSchema);
+
+module.exports = mongoose.model('Project', projectSchema);
