f1db4c5e724ef77fce362f526cfc35df4b4f1d4c
fortuneofweb3 <138443589+fortuneofweb3@users.noreply.github.com>
2025-06-28 05:59:15 +0100

    Update project.js

--- models/project.js
+++ models/project.js
@@ -1,10 +1,15 @@
 const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
 
-const projectSchema = new Schema({
+const projectSchema = new mongoose.Schema({
   name: { type: String, required: true, unique: true },
   keywords: { type: [String], default: [] },
   description: { type: String, default: '' },
+  twitterUsername: { type: String, default: '' },
+  userId: { type: String, default: '' },
+  profile_image_url: { type: String, default: '' },
+  name: { type: String, default: '' },
+  followers_count: { type: Number, default: 0 },
+  following_count: { type: Number, default: 0 },
   updatedAt: { type: Date, default: Date.now }
 });
 
